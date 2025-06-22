04256ccd86fbae457d3b471d10bd4772770adfe0
Roman Sushevskij <roma.sushevskij@yandex.ru>
2025-06-22 17:45:29 +0300

    fix: исправлен конфиг Vite для корректного билда на Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { dirname, resolve } from "path";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -10,7 +15,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": resolve(__dirname, "src"),
       node_modules: "./node_modules",
       public: "./public",
     },
